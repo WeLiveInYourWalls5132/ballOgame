00a66ef8d0865806a3e93e9864697148c15d4d04
WeLiveInYourWalls5132 <109475012+WeLiveInYourWalls5132@users.noreply.github.com>
2022-11-08 14:57:58 +0200

    Obama ball game update

--- Assets/controller3D.cs
+++ Assets/controller3D.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class controller3D : MonoBehaviour
 {
-
-
+    public Transform camera;
+    public float speed,boostSpeed;
     private bool isOnGround = false;
     private Rigidbody rigidbody;
     private Vector3 position;
+
+    private float currentSpeed;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -17,18 +19,44 @@ public class controller3D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = boostSpeed;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+        /*
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            rigidbody.AddTorque(Vector3.up*1000);
+
+            var vel = rigidbody.velocity;
+            if(vel.x<0) vel.x = 0;
+            rigidbody.velocity = vel;
+            rigidbody.AddForce(Vector3.right*currentSpeed*Time.deltaTime);
+            
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rigidbody.AddTorque(Vector3.up*-1000);
-        }
+            var vel = rigidbody.velocity;
+            if(vel.x>0) vel.x = 0;
+            rigidbody.velocity = vel;
+            rigidbody.AddForce(-Vector3.right*currentSpeed*Time.deltaTime);
+        }*/
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            rigidbody.AddForce(Vector3.forward*100);
+        if (Input.GetKey(KeyCode.UpArrow) ||  Input.GetKey(KeyCode.W))
+        {var vel = rigidbody.velocity;
+            if(vel.z<0) vel.z = 0;
+            rigidbody.velocity = vel;
+            rigidbody.AddForce((transform.position - camera.position).normalized *currentSpeed*Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.DownArrow) ||  Input.GetKey(KeyCode.S))
+        {var vel = rigidbody.velocity;
+            if(vel.z>0) vel.z = 0;
+            rigidbody.velocity = vel;
+            rigidbody.AddForce(-(transform.position - camera.position).normalized *currentSpeed*Time.deltaTime);
         }
     }
 }
